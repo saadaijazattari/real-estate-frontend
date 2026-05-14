55e9ff7dbde14d84ce028d48f3d8b81d74847f62
saadaijazattari <saad.aijaz1234@gmail.com>
2026-05-14 11:57:10 +0500

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './layout.scss'
 import Navbar from './components/Navbar/Navbar'
+import HomePage from './routes/homePage/homePage'
 
 const App = () => {
   return (
     <div className='layout'>
+      <div className="navbar">
       <Navbar/>
+
+      </div>
+
+      <div className="content">
+      <HomePage/>
+      </div>
     </div>
   )
 }
---
+++ src/routes/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomePage.scss'
+
+const HomePage = () => {
+  return (
+    <div className='homePage'>
+        <div className="textContainer">left</div>
+        <div className="imgContainer">right</div>
+    </div>
+  )
+}
+
+export default HomePage
